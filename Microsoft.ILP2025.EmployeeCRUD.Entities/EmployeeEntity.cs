3933abccc983645ae67f18bb6552c452b8b28ff8
Shubhranshu Bhardwaj <bhardwajshubhranshu@gmail.com>
2025-06-19 07:37:29 +0530

    first commit

---
+++ Microsoft.ILP2025.EmployeeCRUD.Entities/EmployeeEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.ILP2025.EmployeeCRUD.Entities
+{
+    public class EmployeeEntity
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string Departmet { get; set; }
+
+        public string Designation { get; set; }
+
+        public DateTime DateOfJoing { get; set; }
+    }
+}
